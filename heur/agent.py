8840e2d21ebb9836bad31de1aff772936ec08743
Maciej Sypetkowski <maciej.sypetkowski@gmail.com>
2021-09-05 18:47:45 +0000

    Make compatible with nle==0.7.3

--- heur/agent.py
+++ heur/agent.py
@@ -20,7 +20,7 @@ from monster_tracker import MonsterTracker
 from strategy import Strategy
 
 BLStats = namedtuple('BLStats',
-                     'x y strength_percentage strength dexterity constitution intelligence wisdom charisma score hitpoints max_hitpoints depth gold energy max_energy armor_class monster_level experience_level experience_points time hunger_state carrying_capacity dungeon_number level_number')
+                     'x y strength_percentage strength dexterity constitution intelligence wisdom charisma score hitpoints max_hitpoints depth gold energy max_energy armor_class monster_level experience_level experience_points time hunger_state carrying_capacity dungeon_number level_number prop_mask')
 
 
 class Agent:
