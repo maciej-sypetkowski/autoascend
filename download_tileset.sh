6f0689a042295cebcb63df7b9ef41e2449d9b2ba
Maciej Sypetkowski <maciej.sypetkowski@gmail.com>
2022-02-26 12:12:56 +0100

    Fix heur -> src references

--- download_tileset.sh
+++ download_tileset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR=$(dirname "$0")
-mkdir -p "$DIR"/heur/tilesets
-wget 'https://nethackwiki.com/mediawiki/images/7/73/3.6.1tiles32.png' -P "$DIR"/heur/tilesets
+mkdir -p "$DIR"/src/tilesets
+wget 'https://nethackwiki.com/mediawiki/images/7/73/3.6.1tiles32.png' -P "$DIR"/src/tilesets
