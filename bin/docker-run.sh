678450705a73935066d10e2527a1d0bdc723b465
Maciej Sypetkowski <maciej.sypetkowski@gmail.com>
2022-03-08 23:53:52 +0100

    Remove unnecessary mounts in docker-run.sh

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 devices=0
-# TODO: mounts
 docker run --gpus='"device='$devices'"' --ipc=host --net=host -it --rm \
     -e DISPLAY=$DISPLAY \
     -e PYTHONPATH=/workspace \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.Xauthority:/root/.Xauthority \
-    -v /tmp/vis:/tmp/vis \
-    -v /data2/checkpoints/nethack:/checkpoints \
     -v `pwd`:/workspace nethack:latest "$@"
